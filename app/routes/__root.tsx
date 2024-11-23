ad307cc940548c63e39da9be4ed706a5e21d0190
Ben Durrant <ben.j.durrant@gmail.com>
2024-11-23 17:39:59 +0000

    Make root loader into a server function

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,23 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   Outlet,
   ScrollRestoration,
   createRootRouteWithContext,
 } from "@tanstack/react-router";
-import { Meta, Scripts } from "@tanstack/start";
+import { createServerFn, Meta, Scripts } from "@tanstack/start";
 import type { ReactNode } from "react";
 import { lazily } from "react-lazily";
+import { ensureAuthenticatedMw } from "@/middleware/auth";
+import { queryClientMw } from "@/middleware/query-client";
 import { ForeEauFore } from "~/404";
 import { BreakpointDisplay } from "~/components/grid";
 import { GlobalToastRegion } from "~/components/toast/toast-region";
-import { ensureAuthenticated } from "~/db/auth";
-import { SessionProvider } from "~/db/provider";
+import { SessionProvider, useSession } from "~/db/provider";
 import { ErrorPage } from "~/error-page";
 import type { Profile } from "~/features/profiles";
 import { getProfile } from "~/features/profiles";
 import type { UserConfig } from "~/features/user_config";
 import { getUserConfig } from "~/features/user_config";
 import { useIsomorphicLayoutEffect } from "~/hooks/use-isomorphic-layout-effect";
+import { useOptionsCreator } from "~/hooks/use-options-creator";
 import { getUrl } from "~/util/isomorphic";
 import { promiseOwnProperties } from "~/util/ponyfills";
 import type { AppContext } from "~/util/supabase-query";
@@ -34,24 +37,25 @@ interface RootLoaderResponse {
   profile?: Profile;
 }
 
+const getUserData = createServerFn({ method: "GET" })
+  .middleware([ensureAuthenticatedMw, queryClientMw])
+  .handler(
+    async ({ context, context: { queryClient, user } }) =>
+      promiseOwnProperties({
+        profile: queryClient.ensureQueryData(getProfile(context, user.id)),
+        config: queryClient.ensureQueryData(getUserConfig(context, user.id)),
+      }) satisfies Promise<RootLoaderResponse>,
+  );
+
 const noAuthRoutes = new Set(["/sign-in", "/auth/callback"]);
 
 export const Route = createRootRouteWithContext<AppContext>()({
-  loader: async ({
-    context,
-    context: { queryClient },
-  }): Promise<RootLoaderResponse> => {
+  loader: async (): Promise<RootLoaderResponse> => {
     const url = await getUrl();
     if (noAuthRoutes.has(url.pathname)) {
       return {};
     }
-    const user = await ensureAuthenticated(context);
-    return {
-      ...(await promiseOwnProperties({
-        profile: queryClient.ensureQueryData(getProfile(context, user.id)),
-        config: queryClient.ensureQueryData(getUserConfig(context, user.id)),
-      })),
-    };
+    return getUserData();
   },
   head: () => ({
     meta: [
@@ -96,7 +100,12 @@ const applyConfig = (el: HTMLElement, config: Nullish<UserConfig>) => {
 };
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  const { config } = Route.useLoaderData();
+  const { config: prefetchConfig } = Route.useLoaderData();
+  const session = useSession();
+  const { data: config } = useQuery({
+    ...useOptionsCreator(getUserConfig, session?.user.id),
+    initialData: prefetchConfig,
+  });
   useIsomorphicLayoutEffect(() => {
     applyConfig(document.documentElement, config);
   }, [config]);
