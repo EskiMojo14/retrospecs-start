a253db87d70d6b39788ec34fe7f946f7425ad219
Ben Durrant <ben.j.durrant@gmail.com>
2024-11-23 21:13:29 +0000

    fix redirect handling

--- app/routes/auth.callback.tsx
+++ app/routes/auth.callback.tsx
@@ -1,47 +1,36 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, isRedirect, redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
 import { getRequestURL } from "vinxi/http";
 import { createServerClient } from "~/db/server";
+
+const redirectToErr = (error?: string, error_description?: string) =>
+  redirect({
+    to: `/sign-in`,
+    search: { error, error_description },
+  });
 
 const authCallback = createServerFn().handler(async () => {
   const { searchParams } = getRequestURL();
   const error = searchParams.get("error") ?? undefined;
   const errorDescription = searchParams.get("error_description") ?? undefined;
-  if (error ?? errorDescription) {
-    throw redirect({
-      to: `/sign-in`,
-      search: { error, error_description: errorDescription },
-    });
-  }
+  if (error ?? errorDescription) throw redirectToErr(error, errorDescription);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
   const supabase = createServerClient();
-  if (code) {
-    try {
-      const { error } = await supabase.auth.exchangeCodeForSession(code);
-      if (!error) {
-        throw redirect({
-          to: next,
-        });
-      }
-      throw redirect({
-        to: `/sign-in`,
-        search: { error: error.message },
-      });
-    } catch (error) {
-      console.error(error);
-      throw redirect({
-        to: `/sign-in`,
-        search: {
-          error: error instanceof Error ? error.message : String(error),
-        },
-      });
-    }
+  if (!code) throw redirectToErr("No code provided");
+  try {
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (error) throw redirectToErr(error.message);
+    throw redirect({ to: next });
+  } catch (error) {
+    if (isRedirect(error)) throw error;
+
+    console.error(error);
+    throw redirectToErr(
+      "An error occurred",
+      error instanceof Error ? error.message : String(error),
+    );
   }
-  throw redirect({
-    to: `/sign-in`,
-    search: { error: "No code provided" },
-  });
 });
 
 export const Route = createFileRoute("/auth/callback")({
