2566a1cfbbb5a310a36eae1d71da5c56be8d3ca4
Ben Durrant <ben.durrant@marketdojo.com>
2024-11-28 12:12:53 +0000

    Tweak expectUsesNative

--- src/util/ponyfills.test.ts
+++ src/util/ponyfills.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { describe, expect, it, vi } from "vitest";
+import type * as Ponyfills from "./ponyfills";
 import {
   mapGetOrInsert,
   mapGetOrInsertComputed,
@@ -11,6 +12,8 @@ import {
 } from "./ponyfills";
 import type { MaybePromise } from "./types";
 
+type Ponyfills = typeof Ponyfills;
+
 /**
  * Test that the ponyfill uses the native implementation if available.
  *
@@ -18,27 +21,24 @@ import type { MaybePromise } from "./types";
  *
  * Anything modified in setup should be restored in cleanup.
  */
-function expectUsesNative<Context extends {}>(
-  description: string,
+function expectUsesNative<Context extends {}, K extends keyof Ponyfills>(
+  nativeName: string,
+  ponyfillName: K,
   {
     setup,
     run,
     cleanup,
   }: {
     setup: () => Context;
-    run: (
-      // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-      module: typeof import("./ponyfills"),
-      context: Context,
-    ) => MaybePromise<void>;
+    run: (fn: Ponyfills[K], context: Context) => MaybePromise<void>;
     cleanup: (context: Context) => void;
   },
 ) {
   // eslint-disable-next-line vitest/expect-expect
-  it(description, async () => {
+  it(`calls native ${nativeName} if available`, async () => {
     const context = setup();
     vi.resetModules();
-    await run(await import("./ponyfills"), context);
+    await run((await import("./ponyfills"))[ponyfillName], context);
     cleanup(context);
   });
 }
@@ -82,7 +82,7 @@ describe("utils > ponyfills", () => {
       expect(map.get(key)).toBe(value);
       expect(result2).toBe(value);
     });
-    expectUsesNative("should call native getOrInsert if available", {
+    expectUsesNative("Map.prototype.getOrInsert", "mapGetOrInsert", {
       setup() {
         const original = Map.prototype.getOrInsert;
 
@@ -92,12 +92,12 @@ describe("utils > ponyfills", () => {
 
         return { mock, original };
       },
-      run(m, { mock }) {
+      run(mapGetOrInsert, { mock }) {
         const map = new Map<string, number>();
         const key = "foo";
         const value = 42;
 
-        m.mapGetOrInsert(map, key, value);
+        mapGetOrInsert(map, key, value);
 
         expect(mock).toHaveBeenCalledWith(key, value);
         expect(mock).toHaveBeenCalledWithContext(map);
@@ -106,54 +106,58 @@ describe("utils > ponyfills", () => {
         Map.prototype.getOrInsert = original;
       },
     });
-    describe("mapGetOrInsertComputed", () => {
-      it("should insert a value if it does not exist", () => {
-        const map = new Map<string, number>();
-        const key = "foo";
-        const value = 42;
-        expect(map.has(key)).toBe(false);
+  });
+  describe("mapGetOrInsertComputed", () => {
+    it("should insert a value if it does not exist", () => {
+      const map = new Map<string, number>();
+      const key = "foo";
+      const value = 42;
+      expect(map.has(key)).toBe(false);
 
-        const insert = vi.fn(() => value);
+      const insert = vi.fn(() => value);
 
-        const result = mapGetOrInsertComputed(map, key, insert);
+      const result = mapGetOrInsertComputed(map, key, insert);
 
-        expect(map.get(key)).toBe(value);
-        expect(result).toBe(value);
-        expect(insert).toHaveBeenCalledWith(key);
-      });
-      it("should return the value if it exists", () => {
-        const map = new Map<string, number>([["foo", 42]]);
-        const key = "foo";
-        const value = 42;
-        expect(map.has(key)).toBe(true);
+      expect(map.get(key)).toBe(value);
+      expect(result).toBe(value);
+      expect(insert).toHaveBeenCalledWith(key);
+    });
+    it("should return the value if it exists", () => {
+      const map = new Map<string, number>([["foo", 42]]);
+      const key = "foo";
+      const value = 42;
+      expect(map.has(key)).toBe(true);
 
-        const insert = vi.fn(() => 0);
+      const insert = vi.fn(() => 0);
 
-        const result = mapGetOrInsertComputed(map, key, insert);
+      const result = mapGetOrInsertComputed(map, key, insert);
 
-        expect(result).toBe(value);
-        expect(insert).not.toHaveBeenCalled();
-      });
-      it("should work with weakmaps", () => {
-        const map = new WeakMap<object, number>();
-        const key = {};
-        const value = 42;
-        expect(map.has(key)).toBe(false);
+      expect(result).toBe(value);
+      expect(insert).not.toHaveBeenCalled();
+    });
+    it("should work with weakmaps", () => {
+      const map = new WeakMap<object, number>();
+      const key = {};
+      const value = 42;
+      expect(map.has(key)).toBe(false);
 
-        const insert = vi.fn(() => value);
+      const insert = vi.fn(() => value);
 
-        const result = mapGetOrInsertComputed(map, key, insert);
+      const result = mapGetOrInsertComputed(map, key, insert);
 
-        expect(map.get(key)).toBe(value);
-        expect(result).toBe(value);
-        expect(insert).toHaveBeenCalledWith(key);
+      expect(map.get(key)).toBe(value);
+      expect(result).toBe(value);
+      expect(insert).toHaveBeenCalledWith(key);
 
-        const result2 = mapGetOrInsertComputed(map, key, () => 0);
+      const result2 = mapGetOrInsertComputed(map, key, () => 0);
 
-        expect(map.get(key)).toBe(value);
-        expect(result2).toBe(value);
-      });
-      expectUsesNative("should call native getOrInsertComputed if available", {
+      expect(map.get(key)).toBe(value);
+      expect(result2).toBe(value);
+    });
+    expectUsesNative(
+      "Map.prototype.getOrInsertComputed",
+      "mapGetOrInsertComputed",
+      {
         setup() {
           const original = Map.prototype.getOrInsertComputed;
 
@@ -163,12 +167,12 @@ describe("utils > ponyfills", () => {
 
           return { mock, original };
         },
-        run(m, { mock }) {
+        run(mapGetOrInsertComputed, { mock }) {
           const map = new Map<string, number>();
           const key = "foo";
           const value = 42;
 
-          m.mapGetOrInsertComputed(map, key, () => value);
+          mapGetOrInsertComputed(map, key, () => value);
 
           expect(mock).toHaveBeenCalledWith(key, expect.any(Function));
           expect(mock).toHaveBeenCalledWithContext(map);
@@ -176,206 +180,200 @@ describe("utils > ponyfills", () => {
         cleanup({ original }) {
           Map.prototype.getOrInsertComputed = original;
         },
-      });
-    });
-    describe("mapGroupBy", () => {
-      it("should group by the key", () => {
-        const items = [
-          { id: 1, name: "foo" },
-          { id: 2, name: "foo" },
-          { id: 3, name: "bar" },
-        ];
-        const result = mapGroupBy(items, (item) => item.name);
-        expect(result).toEqual(
-          new Map([
-            ["foo", [items[0], items[1]]],
-            ["bar", [items[2]]],
-          ]),
-        );
-      });
-      expectUsesNative("should call native Map.groupBy if available", {
-        setup() {
-          const mock = vi.fn();
-
-          const original = Map.groupBy;
-
-          Map.groupBy = mock;
-
-          return { mock, original };
-        },
-        run(m, { mock }) {
-          const items = [1];
-
-          m.mapGroupBy(items, (item) => item);
-
-          expect(mock).toHaveBeenCalledWith(items, expect.any(Function));
-          expect(mock).toHaveBeenCalledWithContext(Map);
-        },
-        cleanup({ original }) {
-          Map.groupBy = original;
-        },
-      });
-    });
-    describe("objectGroupBy", () => {
-      it("should group by the key", () => {
-        const items = [
-          { id: 1, name: "foo" },
-          { id: 2, name: "foo" },
-          { id: 3, name: "bar" },
-        ];
-        const result = objectGroupBy(items, (item) => item.name);
-        expect(result).toEqual({ foo: [items[0], items[1]], bar: [items[2]] });
-      });
-      expectUsesNative("should call native Object.groupBy if available", {
-        setup() {
-          const mock = vi.fn();
-
-          const original = Object.groupBy;
-
-          Object.groupBy = mock;
-
-          return { mock, original };
-        },
-        run(m, { mock }) {
-          const items = [1];
-
-          m.objectGroupBy(items, (item) => item);
-
-          expect(mock).toHaveBeenCalledWith(items, expect.any(Function));
-          expect(mock).toHaveBeenCalledWithContext(Object);
-        },
-        cleanup({ original }) {
-          Object.groupBy = original;
-        },
-      });
-    });
-    describe("promiseFromEntries", () => {
-      it("should resolve with the entries", async () => {
-        const map = {
-          foo: 1,
-          bar: Promise.resolve(2),
-        };
-        const promise = promiseFromEntries(Object.entries(map));
-        expect(promise).toBeInstanceOf(Promise);
-        await expect(promise).resolves.toEqual({ foo: 1, bar: 2 });
-      });
-      it("should throw if any entry rejects", async () => {
-        const map = {
-          foo: 1,
-          bar: Promise.reject(new Error("bar")),
-        };
-        const promise = promiseFromEntries(Object.entries(map));
-        expect(promise).toBeInstanceOf(Promise);
-        await expect(promise).rejects.toThrow("bar");
-      });
-      expectUsesNative("should call native Promise.fromEntries if available", {
-        setup() {
-          const original = Promise.fromEntries;
-
-          const mock = vi.fn();
-
-          Promise.fromEntries = mock;
-
-          return { mock, original };
-        },
-        async run(m, { mock }) {
-          const entries = [["foo", 1]] as const;
-
-          await m.promiseFromEntries(entries);
-
-          expect(mock).toHaveBeenCalledWith(entries);
-          expect(mock).toHaveBeenCalledWithContext(Promise);
-        },
-        cleanup({ original }) {
-          Promise.fromEntries = original;
-        },
-      });
-    });
-    describe("promiseOwnProperties", () => {
-      it("should resolve with the resolved properties", async () => {
-        const obj = {
-          foo: Promise.resolve(1),
-          bar: Promise.resolve(2),
-        };
-        const promise = promiseOwnProperties(obj);
-        expect(promise).toBeInstanceOf(Promise);
-        await expect(promise).resolves.toEqual({ foo: 1, bar: 2 });
-      });
-      it("should reject if any property rejects", async () => {
-        const obj = {
-          foo: Promise.resolve(1),
-          bar: Promise.reject(new Error("bar")),
-        };
-        const promise = promiseOwnProperties(obj);
-        expect(promise).toBeInstanceOf(Promise);
-        await expect(promise).rejects.toThrow("bar");
-      });
-      expectUsesNative(
-        "should call native Promise.ownProperties if available",
-        {
-          setup() {
-            const original = Promise.ownProperties;
-
-            const mock = vi.fn();
-
-            Promise.ownProperties = mock;
-
-            return { mock, original };
-          },
-          async run(m, { mock }) {
-            const obj = { foo: 1 };
-
-            await m.promiseOwnProperties(obj);
-
-            expect(mock).toHaveBeenCalledWith(obj);
-            expect(mock).toHaveBeenCalledWithContext(Promise);
-          },
-          cleanup({ original }) {
-            Promise.ownProperties = original;
-          },
-        },
+      },
+    );
+  });
+  describe("mapGroupBy", () => {
+    it("should group by the key", () => {
+      const items = [
+        { id: 1, name: "foo" },
+        { id: 2, name: "foo" },
+        { id: 3, name: "bar" },
+      ];
+      const result = mapGroupBy(items, (item) => item.name);
+      expect(result).toEqual(
+        new Map([
+          ["foo", [items[0], items[1]]],
+          ["bar", [items[2]]],
+        ]),
       );
     });
-    describe("promiseWithResolvers", () => {
-      it("should resolve with the resolvers", async () => {
-        const { promise, resolve, reject } = promiseWithResolvers<number>();
-        expect(promise).toBeInstanceOf(Promise);
-        expect(resolve).toBeInstanceOf(Function);
-        expect(reject).toBeInstanceOf(Function);
-        resolve(42);
-        await expect(promise).resolves.toBe(42);
-      });
-      it("should reject with the resolvers", async () => {
-        const { promise, resolve, reject } = promiseWithResolvers<number>();
-        expect(promise).toBeInstanceOf(Promise);
-        expect(resolve).toBeInstanceOf(Function);
-        expect(reject).toBeInstanceOf(Function);
-        reject(new Error("foo"));
-        await expect(promise).rejects.toThrow("foo");
-      });
-      expectUsesNative(
-        "should call native Promise.withResolvers if available",
-        {
-          setup() {
-            const original = Promise.withResolvers;
+    expectUsesNative("Map.groupBy", "mapGroupBy", {
+      setup() {
+        const mock = vi.fn();
 
-            const mock = vi.fn();
+        const original = Map.groupBy;
 
-            Promise.withResolvers = mock;
+        Map.groupBy = mock;
 
-            return { mock, original };
-          },
-          run(m, { mock }) {
-            m.promiseWithResolvers();
+        return { mock, original };
+      },
+      run(mapGroupBy, { mock }) {
+        const items = [1];
 
-            expect(mock).toHaveBeenCalled();
-            expect(mock).toHaveBeenCalledWithContext(Promise);
-          },
-          cleanup({ original }) {
-            Promise.withResolvers = original;
-          },
-        },
-      );
+        mapGroupBy(items, (item) => item);
+
+        expect(mock).toHaveBeenCalledWith(items, expect.any(Function));
+        expect(mock).toHaveBeenCalledWithContext(Map);
+      },
+      cleanup({ original }) {
+        Map.groupBy = original;
+      },
+    });
+  });
+  describe("objectGroupBy", () => {
+    it("should group by the key", () => {
+      const items = [
+        { id: 1, name: "foo" },
+        { id: 2, name: "foo" },
+        { id: 3, name: "bar" },
+      ];
+      const result = objectGroupBy(items, (item) => item.name);
+      expect(result).toEqual({ foo: [items[0], items[1]], bar: [items[2]] });
+    });
+    expectUsesNative("Object.groupBy", "objectGroupBy", {
+      setup() {
+        const mock = vi.fn();
+
+        const original = Object.groupBy;
+
+        Object.groupBy = mock;
+
+        return { mock, original };
+      },
+      run(objectGroupBy, { mock }) {
+        const items = [1];
+
+        objectGroupBy(items, (item) => item);
+
+        expect(mock).toHaveBeenCalledWith(items, expect.any(Function));
+        expect(mock).toHaveBeenCalledWithContext(Object);
+      },
+      cleanup({ original }) {
+        Object.groupBy = original;
+      },
+    });
+  });
+  describe("promiseFromEntries", () => {
+    it("should resolve with the entries", async () => {
+      const map = {
+        foo: 1,
+        bar: Promise.resolve(2),
+      };
+      const promise = promiseFromEntries(Object.entries(map));
+      expect(promise).toBeInstanceOf(Promise);
+      await expect(promise).resolves.toEqual({ foo: 1, bar: 2 });
+    });
+    it("should throw if any entry rejects", async () => {
+      const map = {
+        foo: 1,
+        bar: Promise.reject(new Error("bar")),
+      };
+      const promise = promiseFromEntries(Object.entries(map));
+      expect(promise).toBeInstanceOf(Promise);
+      await expect(promise).rejects.toThrow("bar");
+    });
+    expectUsesNative("Promise.fromEntries", "promiseFromEntries", {
+      setup() {
+        const original = Promise.fromEntries;
+
+        const mock = vi.fn();
+
+        Promise.fromEntries = mock;
+
+        return { mock, original };
+      },
+      async run(promiseFromEntries, { mock }) {
+        const entries = [["foo", 1]] as const;
+
+        await promiseFromEntries(entries);
+
+        expect(mock).toHaveBeenCalledWith(entries);
+        expect(mock).toHaveBeenCalledWithContext(Promise);
+      },
+      cleanup({ original }) {
+        Promise.fromEntries = original;
+      },
+    });
+  });
+  describe("promiseOwnProperties", () => {
+    it("should resolve with the resolved properties", async () => {
+      const obj = {
+        foo: Promise.resolve(1),
+        bar: Promise.resolve(2),
+      };
+      const promise = promiseOwnProperties(obj);
+      expect(promise).toBeInstanceOf(Promise);
+      await expect(promise).resolves.toEqual({ foo: 1, bar: 2 });
+    });
+    it("should reject if any property rejects", async () => {
+      const obj = {
+        foo: Promise.resolve(1),
+        bar: Promise.reject(new Error("bar")),
+      };
+      const promise = promiseOwnProperties(obj);
+      expect(promise).toBeInstanceOf(Promise);
+      await expect(promise).rejects.toThrow("bar");
+    });
+    expectUsesNative("Promise.ownProperties", "promiseOwnProperties", {
+      setup() {
+        const original = Promise.ownProperties;
+
+        const mock = vi.fn();
+
+        Promise.ownProperties = mock;
+
+        return { mock, original };
+      },
+      async run(promiseOwnProperties, { mock }) {
+        const obj = { foo: 1 };
+
+        await promiseOwnProperties(obj);
+
+        expect(mock).toHaveBeenCalledWith(obj);
+        expect(mock).toHaveBeenCalledWithContext(Promise);
+      },
+      cleanup({ original }) {
+        Promise.ownProperties = original;
+      },
+    });
+  });
+  describe("promiseWithResolvers", () => {
+    it("should resolve with the resolvers", async () => {
+      const { promise, resolve, reject } = promiseWithResolvers<number>();
+      expect(promise).toBeInstanceOf(Promise);
+      expect(resolve).toBeInstanceOf(Function);
+      expect(reject).toBeInstanceOf(Function);
+      resolve(42);
+      await expect(promise).resolves.toBe(42);
+    });
+    it("should reject with the resolvers", async () => {
+      const { promise, resolve, reject } = promiseWithResolvers<number>();
+      expect(promise).toBeInstanceOf(Promise);
+      expect(resolve).toBeInstanceOf(Function);
+      expect(reject).toBeInstanceOf(Function);
+      reject(new Error("foo"));
+      await expect(promise).rejects.toThrow("foo");
+    });
+    expectUsesNative("Promise.withResolvers", "promiseWithResolvers", {
+      setup() {
+        const original = Promise.withResolvers;
+
+        const mock = vi.fn();
+
+        Promise.withResolvers = mock;
+
+        return { mock, original };
+      },
+      run(promiseWithResolvers, { mock }) {
+        promiseWithResolvers();
+
+        expect(mock).toHaveBeenCalled();
+        expect(mock).toHaveBeenCalledWithContext(Promise);
+      },
+      cleanup({ original }) {
+        Promise.withResolvers = original;
+      },
     });
   });
 });
