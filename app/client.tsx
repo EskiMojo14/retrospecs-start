eac923da4ed0534a41ef2003ad2c235215215592
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-27 20:39:08 +0000

    add strict mode

--- app/client.tsx
+++ app/client.tsx
@@ -1,9 +1,15 @@
 /// <reference types="vinxi/types/client" />
 import { StartClient } from "@tanstack/react-start";
+import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { createBrowserClient } from "~/db/client";
 import { createRouterCreator } from "./router";
 
 const router = createRouterCreator(createBrowserClient)();
 
-hydrateRoot(document, <StartClient router={router} />);
+hydrateRoot(
+  document,
+  <StrictMode>
+    <StartClient router={router} />
+  </StrictMode>,
+);
